57ffaabd0bf497bc5c37e3255d261891f21f6bc0
viktoriaschuster <viki.schuster@web.de>
2025-03-10 09:36:59 -0400

    sync source

--- src/functions/sae_training.py
+++ src/functions/sae_training.py
@@ -6,7 +6,7 @@ from src.models.sparse_autoencoder import SparseAutoencoder, loss_function, TopK
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def train_sae(train_loader, input_size, hidden_factor, lr, l1_weight=1e-3, sparsity_penalty=0, n_epochs=500, sae_type='vanilla', k_latent_percent=100, return_all_losses=False, print_progress=False):
+def train_sae(train_loader, input_size, hidden_factor, lr, l1_weight=1e-3, sparsity_penalty=0, val_loader=None, n_epochs=500, sae_type='vanilla', k_latent_percent=100, return_all_losses=False, print_progress=False, early_stopping=None):
     # Initialize model
     hidden_size = input_size * hidden_factor
     k_latent = max(1, int(hidden_size * k_latent_percent / 100)) # make sure there is at least 1 latent feature
@@ -25,6 +25,8 @@ def train_sae(train_loader, input_size, hidden_factor, lr, l1_weight=1e-3, spars
 
     # Training loop
     losses = []
+    if val_loader is not None:
+        val_losses = []
     if print_progress:
         epoch_range = tqdm(range(n_epochs))
     else:
@@ -64,8 +66,50 @@ def train_sae(train_loader, input_size, hidden_factor, lr, l1_weight=1e-3, spars
             
             total_loss += loss.item()
         
+        if val_loader is not None:
+            total_val_loss = 0
+            for x in val_loader:
+                inputs = x
+                inputs = inputs.to(device)
+                outputs, encoded = sae_model(inputs)
+                # Compute loss
+                if sae_type != 'vanilla':
+                    loss = loss_function(
+                        outputs, 
+                        inputs, 
+                        encoded, 
+                        sae_model.encoder[1].weight,  # Pass the encoder's weights for weight sparsity
+                        sparsity_penalty, 
+                        l1_weight
+                    )
+                else:
+                    loss = loss_function(
+                        outputs, 
+                        inputs, 
+                        encoded, 
+                        sae_model.encoder[0].weight,  # Pass the encoder's weights for weight sparsity
+                        sparsity_penalty, 
+                        l1_weight
+                    )
+                
+                total_val_loss += loss.item()
+        
         losses.append(total_loss / len(train_loader))
+
+        if val_loader is not None:
+            val_losses.append(total_val_loss / len(val_loader))
+
+        # early stopping
+        if early_stopping is not None:
+            if epoch > early_stopping:
+                # check if we have achieved a new minimum within the last early_stopping epochs
+                if min(val_losses[-early_stopping:]) > min(val_losses):
+                    print("Early stopping at epoch ", epoch)
+                    break
     
     if return_all_losses:
-        return sae_model, losses
+        if val_loader is not None:
+            return sae_model, losses, val_losses
+        else:
+            return sae_model, losses
     return sae_model, losses[-1]
